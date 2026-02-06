197492a05aa911a3272851141a7dd3b704d2d8d3
crhp987 <poornimadl636@gmail.com>
2026-02-06 13:04:47 +0530

    this is my day5 programs

---
+++ day5/main.py
@@ -0,0 +1,5 @@
+import calculator
+a = int(input("enter the first number: "))
+b = int(input("enter the first number: ")) 
+print("addition:",calculator.add(a,b))
+print("subtraction:",calculator.sub(a,b))
